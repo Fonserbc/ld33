bcb4eba8964f23e2ea65dc314cbc435efc297226
Fonserbc <Fonserbc@gmail.com>
2015-08-24 00:18:15 +0200

    Logo and intro added

---
+++ Sticky-Unity/Assets/Scripts/Animationflag.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animationflag : MonoBehaviour {
+
+	public bool flag;
+	
+	// Update is called once per frame
+	void Update () {
+		if (flag) {
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}
